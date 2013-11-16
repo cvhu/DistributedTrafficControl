1b6fbaa6d3fcc5aabcd19bcb3512d1f20a022451
cvhu <vic@cvhu.org>
2013-11-15 18:44:18 -0600

    up

--- src/main/java/edu/utexas/ece/GridWorld.java
+++ src/main/java/edu/utexas/ece/GridWorld.java
@@ -3,20 +3,11 @@ package edu.utexas.ece;
 import java.util.HashMap;
 
 public class GridWorld {
-
-    // Height and width of the grid
-    private Integer height;
-    private Integer width;
-
     // Components of grid
     private HashMap<Coordinate, IntersectionServer> intersectionsMap;
     private VehicleClient[] vehicles;
 
-    // Constructor
     public GridWorld(Integer height, Integer width, Integer nVehicles) {
-        // Set height and width of grid world
-        this.height = height;
-        this.width = width;
 
         // Initialize intersection map
         this.intersectionsMap = new HashMap<Coordinate, IntersectionServer>();
@@ -40,9 +31,6 @@ public class GridWorld {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!" + IntersectionServer.DURATION_STRAIGHT);
-        
-        GridWorld gridWorld = new GridWorld(8, 8, 16);
-        
+        new GridWorld(8, 8, 16);
     }
 }

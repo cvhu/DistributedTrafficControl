aa523436458f5bef607aa2e7e6db8b3181a5a520
squirem <saddam@localhost>
2013-11-16 13:03:16 -0600

    Added color swap class

---
+++ src/main/java/edu/utexas/ece/BlueRandomSwapFilter.java
@@ -0,0 +1,18 @@
+package edu.utexas.ece;
+
+import java.awt.image.RGBImageFilter;
+
+public class BlueRandomSwapFilter extends RGBImageFilter{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int filterRGB(int x, int y, int rgb) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
